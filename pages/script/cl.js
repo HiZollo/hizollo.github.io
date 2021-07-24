39f9c632f55ee9aa7e7ef1518d7c103ccc2ab3a2
kyometori <ac0xrpfs001@vivladi.net>
2021-07-24 23:28:28 +0800

    add beta 0.10.6 update

--- pages/script/cl.js
+++ pages/script/cl.js
@@ -3,4 +3,11 @@ $(document).ready(() => {
   $(".cl-wrapper-title").click(function() {
     $(this).next().slideToggle();
   });
+
+  $(".dc-spoiler").click(function() {
+    $(this).css({
+      "background-color": "#E5E5E5",
+      "cursor": "default"
+    });
+  })
 });
