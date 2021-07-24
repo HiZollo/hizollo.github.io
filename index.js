df9527e8fbe6ab7b1ca9a8831ce7f7104c3ba4e5
kyometori <ac0xrpfs001@vivladi.net>
2021-07-24 18:03:11 +0800

    rebuild somepart & delete unreal things

--- index.js
+++ index.js
@@ -3,24 +3,32 @@ const loadPage = file => {
 }
 
 const popWindow = file => {
-  window.open (file, "pop", "height=100, width=400, top=250, left=550, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no");
+  window.open (file, 'pop', 'height=100, width=400, top=250, left=550, toolbar=no, menubar=no, scrollbars=no, resizable=no,location=no, status=no');
+}
+
+const request = paras => {
+        const url = location.href;
+        const paraString = url.substring(url.indexOf('?')+1, url.length).split('&');
+        const paraObj = {}
+        for (const str of paraString) {
+          const index = str.indexOf('=')
+        	paraObj[str.substring(0, index).toLowerCase()] = str.substring(index+1, str.length);
+        }
+        const returnValue = paraObj[paras.toLowerCase()];
+        if(typeof(returnValue) == 'undefined'){
+        	return "";
+        }else{
+        	return returnValue;
+        }
 }
 
 $(document).ready(() => {
-  loadPage("pages/mp.html");
+  let page = request('page');
+  if (page === '') page = 'mp';
+
+  loadPage(`pages/${page}.html`);
 
   $("#switch").click(function () {
-    $("#nav").animate({opacity: 'toggle'}, 1000);
+    $("#nav").animate({opacity: 'toggle'}, 500);
   });
 });
-
-/**
-if(moveRight) {
-  $("#nav").animate({right: '+=21%'});
-  $("#content").width('100%');
-} else {
-  $("#nav").animate({right: '-=21%'});
-  $("#content").animate({width: '80%'});
-}
-moveRight = !moveRight;
-**/
